bfd54af9dfe03507b32962488cc63256773fba90
Hansh Muhammed <hanshmhd@gmail.com>
2024-11-29 18:35:39 +0530

    contact page data giving to backend over

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from . models import Contact
 
 # Create your views here.
 def contact(request):
-    return render(request,'contact.html')
+   if request.method == "POST":
+       print(request.POST)
+       name = request.POST.get('name')
+       email = request.POST.get('email')
+       subject = request.POST.get('subject')
+       message = request.POST.get('message')
+
+       contact_entry = Contact(name=name, email=email, subject=subject, message=message)
+       contact_entry.save()
+       
+        
+        
+   return render(request,'contact.html')
