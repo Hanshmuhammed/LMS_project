44d5d3be12cb018b1f2285697d6a2d827166f97e
Hansh Muhammed <hanshmhd@gmail.com>
2024-12-05 22:26:17 +0530

    adding xstudents responce

--- core/models.py
+++ core/models.py
@@ -3,3 +3,13 @@ from django.db import models
 # Create your models here.
 class core(models.Model):
     hey=models.CharField(max_length=50)
+
+
+class Testimonial(models.Model):
+    client_name = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='testimonials/')
+    testimonial_text = models.TextField()
+
+    def __str__(self):
+        return self.client_name
